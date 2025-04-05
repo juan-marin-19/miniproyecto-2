0f3f9514bbaad5271b8be3075f075f5d1e132351
Juan Marin Orozco <juanmarinorozco6@gmail.com>
2025-04-05 16:43:45 -0500

    first working scene

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        SudokuBoard sudoku = new SudokuBoard();
-        sudoku.fillRandomOneNumberPerBlock();
-        sudoku.printBoard();
-    }
-}
---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {launch(args);}
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                Main.class.getResource("home-view.fxml")
+        );
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Sodoku");
+        primaryStage.show();
+    }
+
+
+}
